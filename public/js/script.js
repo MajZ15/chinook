1a59cd02668b8030d683eed8aa90b4fb80ee5340
Maj Lenaršič <lenarsicmaj@gmail.com>
2016-06-16 10:08:18 +0000

    Zadnji commit

--- public/js/script.js
+++ public/js/script.js
@@ -30,6 +30,13 @@ $(document).ready(function() {
         Math.round(data.time / 60000) + ' min | <strong>$' + Math.round(data.price) + '</strong> total)');
     });
   });
+  $('#playlists div span').click(function(){
+    var details = $(this);
+    $.get('/playlist/' + details.parent().attr('playlist'), function(data) {
+      details.html(' (' + data.artists + 'artist' + (data.artists != 1 ? 's' : '') + + ' | ' + data.tracks + 'track' + Math.round(data.price) + ' total</strong> ' +')');  
+    })
+    
+  })
   
   
 });
